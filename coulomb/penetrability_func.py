bba45796b18c8d59499d126096dda6488cecbe73
Alexey <andreyanov.alexey.27@gmail.com>
2020-10-16 17:29:38 +0300

    Commit1

---
+++ coulomb/penetrability_func.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 16 12:51:56 2020
+
+@author: lkst
+"""
+import math
+import cmath
+from mpmath import *
+
+
+def C_l(l, eta, z):
+    F = coulombf(l, eta, z)
+    G = coulombg(l, eta, z)
+    
+    P_l = 1/(F**2 + G**2)
+    delta_l = -cmath.atan(F/G)
+    phi_l = cmath.phase(float(math.gamma(l +1 + j*eta)))
+    return(sqrt(P_l*math.exp(j*(delta_l + phi_l))))
+
+
+def F2(x):
+    return(coulombg(0,0,x))
+
+def C_l_E(E):
+    z1=z2=1
+    el = sqrt(1/137)
+    h = 1
+    M1 = 1875.61 # MeV
+    M2 = 1875.61
+    M = M1*M2/(M1+M2)
+    eta = z1*z2*el**2/(h*sqrt(2*E/M))
+    z = 1
+    C_l(0,eta,z)
+    
+
+plot(C_l_E, [0,10], [0,25])
