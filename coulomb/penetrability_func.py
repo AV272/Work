68e406b0a2123aeb06c94117a20fbc95e00dee5f
Alexey <andreyanov.alexey.27@gmail.com>
2020-11-24 18:25:47 +0300

    commit3

--- coulomb/penetrability_func.py
+++ coulomb/penetrability_func.py
@@ -11,7 +11,7 @@ from mpmath import *
 
 
 def C_l(l, eta, z):
-    F = coulombf(l, eta, z)
+    F = coulombf(l, eta, z) 
     G = coulombg(l, eta, z)
     
     P_l = 1/(F**2 + G**2)
@@ -32,8 +32,17 @@ def C_l_E(E):
     M2 = 1875.61
     M = M1*M2/(M1+M2)
     eta = z1*z2*el**2/(h*sqrt(2*E/M))
-    z = 1
-    C_l(0,eta,0.7)
-    
+    return(abs(C_l(0,eta,0.7)))
 
-plot(C_l_E, [0,10], [0,25])
+    
+def C0(E):
+    C_l_E(0, E)
+def C1(E):
+    C_l_E(1, E)
+def C2(E):
+    C_l_E(2, E)
+def C3(E):
+    C_l_E(3, E)
+
+plot(C_l_E, [0,1500], [0,1])
+#plot([C0,C1,C2,C3], [0,100])
