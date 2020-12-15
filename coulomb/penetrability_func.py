99cf33c2a6a64459483330ade2d542e1dc6fb568
Alexey <andreyanov.alexey.27@gmail.com>
2020-12-15 11:21:50 +0300

    commit5

--- coulomb/penetrability_func.py
+++ coulomb/penetrability_func.py
@@ -7,6 +7,9 @@ Created on Fri Oct 16 12:51:56 2020
 """
 import math
 import cmath
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
 from mpmath import *
 
 
@@ -24,25 +27,28 @@ def C_l(l, eta, z):
 def F2(x):
     return(coulombg(0,0,x))
 
-def C_l_E(E):
+def C_l_E(l,E):
     z1=z2=1
     el = sqrt(1/137)
     h = 1
     M1 = 1875.61 # MeV
     M2 = 1875.61
     M = M1*M2/(M1+M2)
-    eta = z1*z2*el**2/(h*sqrt(2*E/M))
-    return(abs(C_l(0,eta,0.7)))
+    #eta = z1*z2*sqrt(M/E)*0.1574
+    eta = 0.16*sqrt(1/E)
+    z2 = 0.219*sqrt(E)*6.63
+    return(cmath.phase(C_l(l,eta,z2)))
 
     
 def C0(E):
-    C_l_E(0, E)
+    return(C_l_E(0, E))
 def C1(E):
-    C_l_E(1, E)
+    return(C_l_E(1, E))
 def C2(E):
-    C_l_E(2, E)
+    return(C_l_E(2, E))
 def C3(E):
-    C_l_E(3, E)
+    return(C_l_E(3, E))
 
-plot(C_l_E, [0,1500], [0,1])
-#plot([C0,C1,C2,C3], [0,100])
+#plot(C_l_E, [0,1500], [0,1])
+#plot(C_l_E, [0,1500])
+plot([C0,C1,C2,C3], [0,1.5])
